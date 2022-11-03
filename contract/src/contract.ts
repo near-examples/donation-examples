a86697e2ac471fc0f5ef0f3f4eca7a579846d9bb
gagdiez <gagdiez.c@gmail.com>
2022-11-03 01:46:15 +0000

    updated sdk-js

--- contract/src/contract.ts
+++ contract/src/contract.ts
@@ -1,28 +1,29 @@
 import { NearBindgen, near, call, view, initialize, UnorderedMap } from 'near-sdk-js'
+
 import { assert } from './utils'
 import { Donation, STORAGE_COST } from './model'
 
 @NearBindgen({})
 class DonationContract {
   beneficiary: string = "v1.faucet.nonofficial.testnet";
-  donations: UnorderedMap = new UnorderedMap('map-uid-1');
+  donations = new UnorderedMap<bigint>('map-uid-1');
 
-  @initialize({privateFunction: true})
-  init({ beneficiary }:{beneficiary: string}) {
+  @initialize({ privateFunction: true })
+  init({ beneficiary }: { beneficiary: string }) {
     this.beneficiary = beneficiary
   }
 
-  @call({payableFunction: true})
+  @call({ payableFunction: true })
   donate() {
     // Get who is calling the method and how much $NEAR they attached
-    let donor = near.predecessorAccountId(); 
+    let donor = near.predecessorAccountId();
     let donationAmount: bigint = near.attachedDeposit() as bigint;
 
-    let donatedSoFar = this.donations.get(donor) === null? BigInt(0) : BigInt(this.donations.get(donor) as string)
+    let donatedSoFar = this.donations.get(donor, {defaultValue: BigInt(0)})
     let toTransfer = donationAmount;
- 
+
     // This is the user's first donation, lets register it, which increases storage
-    if(donatedSoFar == BigInt(0)) {
+    if (donatedSoFar == BigInt(0)) {
       assert(donationAmount > STORAGE_COST, `Attach at least ${STORAGE_COST} yoctoNEAR`);
 
       // Subtract the storage cost to the amount to transfer
@@ -31,7 +32,7 @@ class DonationContract {
 
     // Persist in storage the amount donated so far
     donatedSoFar += donationAmount
-    this.donations.set(donor, donatedSoFar.toString())
+    this.donations.set(donor, donatedSoFar)
     near.log(`Thank you ${donor} for donating ${donationAmount}! You donated a total of ${donatedSoFar}`);
 
     // Send the money to the beneficiary
@@ -42,34 +43,34 @@ class DonationContract {
     return donatedSoFar.toString()
   }
 
-  @call({privateFunction: true})
+  @call({ privateFunction: true })
   change_beneficiary(beneficiary) {
     this.beneficiary = beneficiary;
   }
 
   @view({})
-  get_beneficiary(){ return this.beneficiary }
+  get_beneficiary(): string { return this.beneficiary }
 
   @view({})
-  number_of_donors() { return this.donations.length }
+  number_of_donors(): number { return this.donations.length }
 
   @view({})
-  get_donations({from_index = 0, limit = 50}: {from_index:number, limit:number}): Donation[] {
-    let ret:Donation[] = []
+  get_donations({ from_index = 0, limit = 50 }: { from_index: number, limit: number }): Donation[] {
+    let ret: Donation[] = []
     let end = Math.min(limit, this.donations.length)
-    for(let i=from_index; i<end; i++){
+    for (let i = from_index; i < end; i++) {
       const account_id: string = this.donations.keys.get(i) as string
-      const donation: Donation = this.get_donation_for_account({account_id})
+      const donation: Donation = this.get_donation_for_account({ account_id })
       ret.push(donation)
     }
     return ret
   }
 
   @view({})
-  get_donation_for_account({account_id}:{account_id:string}): Donation{
-    return new Donation({
+  get_donation_for_account({ account_id }: { account_id: string }): Donation {
+    return {
       account_id,
-      total_amount: this.donations.get(account_id) as string
-    })
+      total_amount: this.donations.get(account_id).toString()
+    }
   }
 }
